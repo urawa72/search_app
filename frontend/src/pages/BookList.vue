3d3c51b76b458b9172ad1a89095ebfd5b9ace397
urawa72 <urawa72@gmail.com>
2019-12-03 23:37:05 +0900

    BookListページ表示修正

--- frontend/src/pages/BookList.vue
+++ frontend/src/pages/BookList.vue
@@ -1,8 +1,16 @@
 <template>
   <div class="book-list">
     <h2>Book List</h2>
-    <div v-for="result in results">
-      {{ result.title }}
+    <div class="book-list__total-num" v-if="results.length > 0">
+      合計：{{ results.length }} 冊
+    </div>
+    <div class="book-list__header">
+      <div class="book-list__header-title">タイトル</div>
+      <div class="book-list__header-price">価格</div>
+    </div>
+    <div class="book-list__row" v-for="result in results">
+      <div class="book-list__title">{{ result.title }}</div>
+      <div class="book-list__price">￥{{ result.price }}</div>
     </div>
   </div>
 </template>
@@ -37,5 +45,34 @@ export default {
 <style lang="scss" scoped>
 .book-list {
   margin-top: 80px;
+  &__total-num {
+    padding: 10px 0;
+    font-size: 16px;
+  }
+  &__header {
+    display: flex;
+    width: 500px;
+    margin: 0 auto;
+    padding: 5px 10px;
+    background-color: #333333;
+    text-align: left;
+    color: white;
+  }
+  &__row {
+    display: flex;
+    width: 500px;
+    margin: 0 auto;
+    padding: 5px 0;
+    border-bottom: 1px solid gray;
+  }
+  &__header-title, &__title {
+    width: 60%;
+    text-align: left;
+  }
+  &__header-price, &__price {
+    width: 40%;
+    text-align: left;
+  }
 }
+</style>
 </style>
